fd84f03f3e9c322c469cb91257be43f83a56562a
Etienne Bechara <et@bechara.ai>
2020-09-08 22:11:00 -0300

    Add BaseEntity extension to ORM id entity

--- source/core/orm/orm.entity/orm.id.entity.ts
+++ source/core/orm/orm.entity/orm.id.entity.ts
@@ -1,7 +1,7 @@
-import { PrimaryKey } from '@mikro-orm/core';
+import { AnyEntity, BaseEntity, PrimaryKey } from '@mikro-orm/core';
 import { v4 } from 'uuid';
 
-export abstract class OrmIdEntity {
+export abstract class OrmIdEntity extends BaseEntity<AnyEntity, 'id'> {
 
   @PrimaryKey()
   public id: string = v4();
